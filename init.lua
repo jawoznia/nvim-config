6f2b8f0c87605b65561fb232a2eb86c14885dcac
Jan Woźniak <jnwznk@gmail.com>
2022-12-04 23:04:07 +0100

    Treesitter in separate file

--- init.lua
+++ init.lua
@@ -17,6 +17,7 @@ require("nullls")
 require('lsp-config.language-servers')
 require("lsp")
 require('lsp-config/cmp-config')
+require('lsp-config/treesitter-config')
 
 -- PLUGINS --
 require("nvim-tree").setup{
@@ -30,21 +31,5 @@ require("lualine").setup({
 	},
 })
 require("nvim-autopairs").setup({})
-
--- Treesitter Plugin Setup
-require("nvim-treesitter.configs").setup({
-	ensure_installed = { "lua", "rust", "toml" },
-	auto_install = true,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	ident = { enable = true },
-	rainbow = {
-		enable = true,
-		extended_mode = true,
-		max_file_lines = nil,
-	},
-})
 
 require("impatient")
