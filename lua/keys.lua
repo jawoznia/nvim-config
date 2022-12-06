b33f5e57a13490198b15c6f747c451eb98faa98c
Jan Woźniak <jnwznk@gmail.com>
2022-12-06 10:21:12 +0100

    <leader>nt to NvimTreeToggle

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,22 +1,19 @@
 --[[ keys.lua ]]
-local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- remap the key usedto leave insert mode
 -- map('i', 'jk', '', {})
-map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+map("n", "<leader>nt", ":NvimTreeToggle<CR>", opts)
 
 -- Telescope config
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+map("n", "<leader>ff", builtin.find_files, {})
+map("n", "<leader>fg", builtin.live_grep, {})
+map("n", "<leader>fb", builtin.buffers, {})
+map("n", "<leader>fh", builtin.help_tags, {})
 --map('n', 'ff', [[:Telescope find_files]], {})
 
 -- Copy to clipboard
-vim.keymap.set("n", "<c-c>", '"*y :let @+=@*<CR>', { noremap = true, silent = true })
-vim.keymap.set("n", "<c-v>", '"+p', { noremap = true, silent = true })
-
--- auto-save toggle
-vim.api.nvim_set_keymap("n", "<leader>n", ":ASToggle<CR>", {})
+map("n", "<c-c>", '"*y :let @+=@*<CR>', { noremap = true, silent = true })
+map("n", "<c-v>", '"+p', { noremap = true, silent = true })
